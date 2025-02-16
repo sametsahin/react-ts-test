62b2d0a8dcde0f17b28c96e8f9bfcd306cee7068
Samet Sahin <sahin@dhcp-172-17-53-106.public.burnaby.ca>
2025-02-15 16:49:32 -0800

    homepage updated

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline">Homepage</h1>
     </div>
   );
 }
